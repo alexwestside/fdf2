7e9e53b04c80805d5c51b6a2987fd6c0bb350489
orizhiy <sklif.85@gmail.com>
2017-03-29 16:55:58 +0300

    norm_6

--- hook.c
+++ hook.c
@@ -82,106 +82,10 @@ int ft_key_hook(int key, t_fdf *fdf, t_mlx *mlx)
 	}
 	if (key == 124 || key == 123 || key == 126 || key == 125 || key == 78 || key == 69)
 		ft_key_hook_1(key, &fdf, &mlx);
-//	if (key == 124)
-//	{
-//		(*mlx).move[0] = 1.5;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 0, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 123)
-//	{
-//		(*mlx).move[0] = -1;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 0, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 126)
-//	{
-//		(*mlx).move[1] = -1;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 0, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 125)
-//	{
-//		(*mlx).move[1] = 1;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 0, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 78)
-//	{
-//		(*mlx).move[2] = 0.91;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 0, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 69)
-//	{
-//		(*mlx).move[2] = 1.09;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 0, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-
 	if (key == 89 || key == 91 || key == 92 || key == 86 || key == 87 || key == 88)
 		ft_key_hook_2(key, &fdf, &mlx);
-
-//	if (key == 89)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 3, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 91)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 4, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 92)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 5, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 86)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 8, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 87)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z,mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 9, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 88)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 10, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
+	if (key == 116 || key == 121)
+		ft_key_hook_3(key, &fdf, &mlx);
 	if (key == 116)
 	{
 		(*mlx).move[4] = 1.1;
@@ -223,54 +127,6 @@ void ft_key_hook_1(int key, t_fdf **fdf, t_mlx **mlx)
 
 void ft_key_hook_2(int key, t_fdf **fdf, t_mlx **mlx)
 {
-//	if (key == 89)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z,mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 3, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 91)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 4, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 92)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 5, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 86)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 8, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 87)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z,mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 9, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
-//	if (key == 88)
-//	{
-//		(*mlx).move[3] = 5.0;
-//		ft_uprising_coord_2(&fdf->map_z, mlx->move);
-//		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 10, mlx->move);
-//		ft_uprising_coord_3(&fdf->map_c);
-//		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
-//	}
 	(*mlx)->move[3] = 5.0;
 	ft_uprising_coord_2(&(*fdf)->map_z, (*mlx)->move);
 	if (key == 89)
@@ -291,7 +147,22 @@ void ft_key_hook_2(int key, t_fdf **fdf, t_mlx **mlx)
 
 void ft_key_hook_3(int key, t_fdf *fdf, t_mlx *mlx)
 {
-
+	if (key == 116)
+	{
+		(*mlx).move[4] = 1.1;
+		ft_uprising_coord_2(&fdf->map_z, mlx->move);
+		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 6, mlx->move);
+		ft_uprising_coord_3(&fdf->map_c);
+		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
+	}
+	if (key == 121)
+	{
+		(*mlx).move[4] = 1.1;
+		ft_uprising_coord_2(&fdf->map_z, mlx->move);
+		ft_matrix_trans_2(&fdf->mlx->angle, &fdf->map_z, &fdf->map_c, 7, mlx->move);
+		ft_uprising_coord_3(&fdf->map_c);
+		ft_put_image(&fdf, W_HIGHT, W_WIDTH);
+	}
 }
 
 void ft_key_hook_4(int key, t_fdf *fdf, t_mlx *mlx)
